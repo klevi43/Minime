fbf1348c02423341ff9c68ee9df8e3e5700e03a9
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-12-17 21:36:46 +0900

    Played more with color pallette in index.css

--- minimi/src/components/Hero/Hero.tsx
+++ minimi/src/components/Hero/Hero.tsx
@@ -1,13 +1,17 @@
 
 import heroImg from '../../assets/Youngson-w-kids-cropped.jpg'
 import './Hero.css';
+
 function Hero() {
   return (
     <>
         <div className="container">
           <div className="grid grid--1x2">
             <img className="hero--image" src={heroImg} alt="" />
-            <div className="box-2">함께 배우자</div>
+            <div className="hero--box">
+              <h2 className="header">함께 배우자</h2>
+              <p>Lorem ipsum do</p>
+            </div>
           </div>
           
         </div>
