c2d0b8c81745b04a6417983d5f433f4a6170676b
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-12-22 19:18:42 +0900

    Basically completed the drop down menu. Need to add a transition and animations to it

--- minimi/src/components/Nav/Nav.tsx
+++ minimi/src/components/Nav/Nav.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import "./Nav.css"
 import logo from "../../assets/logo.png"
 import Icon from "../Icon/Icon"
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavBar = () => {
+      setIsOpen(!isOpen);
+  }
   return (
     <nav className="nav">
       
-      <a href='#'><img className="logo" src={logo}/><span>minimi</span></a>
-      <a href="#"></a>
-      <Icon></Icon>
-      <ul className="list nav__list">
+      <a href='#'><img className="logo" src={logo}/>minimi</a>
+      <button className="hidden" onClick={toggleNavBar}><Icon/></button>
+      <ul className={`list nav__list ${isOpen ? ".collapsible--expanded .collapsible__content" : "collapsible__content"}`}>
         <li className="nav__item"><a href="#">수업 안내</a></li>
         <li className="nav__item"><a href="#">강사 소개</a></li>
         <li className="nav__item"><a href="#">시설</a></li>
