009752288a1915ffb08c9eeb9f4974414d792c60
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-12-26 20:50:56 +0900

    Played around with the hero more. looking for more inspiration on designing a web page.

--- minimi/src/components/Hero/Hero.tsx
+++ minimi/src/components/Hero/Hero.tsx
@@ -1,16 +1,27 @@
 
-import heroImg from '../../assets/Youngson-w-kids-cropped.jpg'
+import heroImg from '../../assets/Youngson-w.-kids.jpg'
 import './Hero.css';
 
 function Hero() {
   return (
     <>
         <div className="container">
-          <div className="grid grid--1x2">
             
-          </div>
+              {/* <h2 className="header hero__text">미니미</h2> */}
+              <div className="centered">
+                <div className="hero__box">
+                  <h3 className="header hero__text">함께</h3>
+                <h3 className="header hero__text">놀자</h3>
+                </div>
+                
+              </div>
+              
           
+              <img className="hero__image" src={heroImg} alt="" />
         </div>
+            
+            
+      
       
     </>
   )
