b8c95f49b3f28e95418b142e95869d60134c3ad2
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-12-29 19:54:11 +0900

    made first hero

---
+++ minimi/src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+      <a className="btn btn-primary" href="#">바로 가기</a>
+    </>
+  )
+}
+
+export default Button
--- minimi/src/components/Hero/Hero.tsx
+++ minimi/src/components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
 
 import heroImg from '../../assets/Youngson-w.-kids.jpg'
-import hero from '../../assets/hero-cropped.jpg'
+import hero from '../../assets/kids-stretching.jpg'
 import hero2 from '../../assets/hero2.jpg'
 import './Hero.css';
+import Button from '../Button/Button';
 
 function Hero() {
   return (
@@ -10,19 +11,28 @@ function Hero() {
         <div className="container">
             
               {/* <h2 className="header hero__text">미니미</h2> */}
-              <div className="centered">
+              <div className="top-right">
                 {/* <h2 className="header hero__text">몸을</h2>
                 <h2 className="header hero__text">튼튼하게</h2>
                 <h2 className="header hero__text">만들자</h2> */}
-                <h2 className="header hero__text">함께</h2>
-                <h2 className="header hero__text">배우자</h2>
+                <h2 className="header hero__header hero__text">함께</h2>
+                <h2 className="header hero__header hero__text">배우자</h2>
                 
               </div>
+              <div className="bottom-left ">
+                <p className="subheader hero__subheader">
+                  <div>K-pop부터</div>
+                  <div>필라테스까지</div>
+                  <span>다!</span> 있습니다.
+                </p>
+              </div>
               
-              {/* <h2 className="header centered">배우자</h2> */}
               
           
               <img className="hero__image" src={hero} alt="" />
+              <div className="bottom-right">
+                <Button/>
+              </div>
         </div>
             
             
