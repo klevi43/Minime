fec156f6a2ac45520d9c14afe13a16673be89689
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-01-02 16:31:23 +0900

    simplified layout of hero and things are starting to shape up better.
    fix button margin

--- minimi/src/components/Hero/Hero.tsx
+++ minimi/src/components/Hero/Hero.tsx
@@ -9,25 +9,30 @@ function Hero() {
   return (
     <>
         <div className="container grid grid--1x2">
-            
-              {/* <h2 className="header hero__text">미니미</h2> */}
-              
-                {/* <h2 className="header hero__text">몸을</h2>
-                <h2 className="header hero__text">튼튼하게</h2>
-                <h2 className="header hero__text">만들자</h2> */}
-                <h2 className="header hero__header">함께 <div>배우자</div></h2>
+            <div className="hero__content">
+              <div className="hero__header-content">
+                <h2 className="header hero__header">
+                  <div>함께</div>
+                  <div>배우자</div>
                 
-             
-            
+                </h2>
+              </div>
+              <div className="hero__subheader-content">
                 <p className="subheader hero__subheader">
                   <div>K-pop부터</div>
                   <div>필라테스까지</div>
                   <span>다!</span> 있습니다.
                 </p>
-              <img className="hero__image" src={hero} alt="" />
-              <div className="hero__button">
+                <div className="hero__button">
                 <Button/>
               </div>
+              
+              </div>
+              
+            </div>
+              
+              <img className="hero__image" src={hero} alt="" />
+              
         </div>
             
             
